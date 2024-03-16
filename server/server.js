68b03af27a4943c0d065abbda8734ad3341d55d2
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2024-03-16 19:46:15 +0200

    Created an .env file and server.js

---
+++ server/server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const routes = require('./routes');
+const dbUri = process.env.DB_URI || 'mongodb://127.0.0.1:27017/book-corner';
+const dbPort = process.env.DB_PORT || '3000';
+
+//const { authMiddleware } = require('./middlewares/authMiddleware') //THIS IS FOR THE NAVIGATION TO WORK
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+//app.use(authMiddleware); //THIS IS FOR THE NAVIGATION TO WORK
+
+app.use(cors({ origin: 'http://localhost:4200' }));
+app.use(routes);
+
+mongoose.connect(dbUri);
+mongoose.connection.on('connected', () => console.log('DB is Connected!'));
+mongoose.connection.on('disconnected', () => console.log('DB is Disconnected!'));
+mongoose.connection.on('error', (err) => console.log('DB Error: ' + err));
+
+app.listen(dbPort, () => console.log(`App is listening on http://localhost:${dbPort}`));
