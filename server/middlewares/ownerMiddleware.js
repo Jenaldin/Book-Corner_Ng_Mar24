3458f0ee04854f02166eeeb34ddafdffd47a1ebb
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2024-03-17 01:16:26 +0200

    Added temporary ownerMiddleware

---
+++ server/middlewares/ownerMiddleware.js
@@ -0,0 +1,10 @@
+//const stonesService = require('../services/stonesService');
+
+exports.isItemOwner = async (req, res, next) => {
+   const item = await stonesService.getOne(req.params.itemId).lean();
+   if (item.owner != req.user?._id) {
+      //return res.redirect('/vault/dashboard');
+   };
+   req.item = item;
+   next();
+};
