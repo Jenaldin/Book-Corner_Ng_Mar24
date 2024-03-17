a21a3f99b6031e9f52b1a8674a1b9814fa8686da
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2024-03-17 14:16:24 +0200

    Created Core and Shared Modules; Added Header and Footer components into Shared Modules; Created a copy of the original app component html

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CoreModule,
+    SharedModule
     BrowserModule,
     AppRoutingModule
   ],
