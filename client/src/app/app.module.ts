e5f25d178ded640a28332174617a1f2d114b1c11
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2024-03-17 05:15:59 +0200

    Rolled back some changes to fix home

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
-;
+import { NgModule } from '@angular/core';
 import { SearchComponent } from './search/search.component';
-import { ErrorComponent } from './error/error.component'import { NgModule } from '@angular/core';
+import { ErrorComponent } from './error/error.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -10,15 +10,14 @@ import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { UserModule } from './user/user.module';
 import { BookModule } from './book/book.module';
-import { BookCatalogComponent } from './book/book-catalog/book-catalog.component';
 import { HomeComponent } from './home/home.component';
-import { 404Component } from './404/404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    404Component, ErrorComponent, SearchComponent,
+    ErrorComponent, 
+    SearchComponent,
   ],
   imports: [
     CoreModule,
