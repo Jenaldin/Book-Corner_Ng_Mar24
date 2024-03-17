f477d0040224612188f1e3561cc38a724c627497
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2024-03-17 16:00:50 +0200

    Added HTTP Client module; Added API service

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,6 +18,7 @@ import { UsersModule } from './users/users.module';
     SharedModule,
     BooksModule,
     UsersModule,
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule
   ],
