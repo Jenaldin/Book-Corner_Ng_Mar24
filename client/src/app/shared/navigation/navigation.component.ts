c1617e3c3c222a9c12cf46b05d5469a174a3d61b
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2024-03-18 00:40:47 +0200

    Created User service ts; Updated Navigation component to show navigation for guests and logged-in

--- client/src/app/shared/navigation/navigation.component.ts
+++ client/src/app/shared/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthUserService } from 'src/app/user.service';
 
 @Component({
   selector: 'app-navigation',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent {
-
+  constructor(public authUserService: AuthUserService) { }
 }
