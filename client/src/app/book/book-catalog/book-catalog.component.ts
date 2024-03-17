82b106cb8c09b93e524a1980e042d79b03028ac3
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2024-03-17 04:26:06 +0200

    Modified book catalog component

--- client/src/app/book/book-catalog/book-catalog.component.ts
+++ client/src/app/book/book-catalog/book-catalog.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-book-catalog',
   templateUrl: './book-catalog.component.html',
   styleUrls: ['./book-catalog.component.scss']
 })
-export class BookCatalogComponent {
+export class BookCatalogComponent implements OnInit{
+  constructor(private api: ApiService) {};
+
+  ngOnInit(): void {
+    this.api.getBooks().subscribe((books) =>{
+      console.log(books);
+      //this.books = books
+    });
+  }
 
 }
