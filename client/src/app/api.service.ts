1922c5ace834779c8c898fee22ec51b9b23c2fcc
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2024-03-17 02:22:46 +0200

    Added HttpClient and created environments

--- client/src/app/api.service.ts
+++ client/src/app/api.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,10 @@ import { Injectable } from '@angular/core';
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getBooks(){
+
+  }
+
 }
