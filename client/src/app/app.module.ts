77db11357a0c62c00634000cc99726760341cd1c
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2024-03-17 14:49:09 +0200

    Added Books module and Users module to App modules

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { BooksModule } from './books/books.module';
+import { UsersModule } from './users/users.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,8 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     CoreModule,
     SharedModule,
+    BooksModule,
+    UsersModule,
     BrowserModule,
     AppRoutingModule
   ],
