9f3116ac36b9ddce6da087a64d2c3f51abcdf210
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2024-03-17 02:15:35 +0200

    Created api service

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,6 +19,7 @@ import { BookModule } from './book/book.module';
     UserModule,
     BookModule,
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
