de72dde965254d2ea7b5d629c827a0a2c76b8beb
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2024-03-17 15:09:20 +0200

    Created Login, Register, Profile components under Users module

--- client/src/app/users/users.module.ts
+++ client/src/app/users/users.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RegisterComponent,
+    LoginComponent,
+    ProfileComponent
+  ],
   imports: [
     CommonModule
   ]
