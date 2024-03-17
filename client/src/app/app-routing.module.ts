b24b2e21723b8f8367e44f0f825e5d52583c751e
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2024-03-17 14:47:04 +0200

    Added Home, About, Search components to app-routing module

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './core/home/home.component';
+import { ErrorComponent } from './core/error/error.component';
+import { AboutComponent } from './core/about/about.component';
+import { SearchComponent } from './core/search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: HomeComponent},
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'search', component: SearchComponent },
+
+  { path: '**', redirectTo: '/404' },
+  { path: '404', component: ErrorComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
