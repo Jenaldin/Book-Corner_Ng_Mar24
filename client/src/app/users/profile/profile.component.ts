754404aa11cda726fa9f3133586dedbdd761be90
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2024-03-22 13:03:26 +0200

    Added basic html and spinner on Login, Register and Profile

--- client/src/app/users/profile/profile.component.ts
+++ client/src/app/users/profile/profile.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  isLoading: boolean = true;
+  
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 1000);
+  }
 
 }
--- client/src/app/users/register/register.component.ts
+++ client/src/app/users/register/register.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  isLoading: boolean = true;
+  
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 1000);
+  }
 
 }
