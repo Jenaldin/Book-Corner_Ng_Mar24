e29d3fedb47380e3e7494683606ba1a53bfa401b
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2024-03-22 17:03:35 +0200

    Added e-mail validator

--- client/src/app/utils/email-valid.ts
+++ client/src/app/utils/email-valid.ts
@@ -1,9 +1,27 @@
-import { ValidatorFn } from '@angular/forms';
+// import { ValidatorFn } from '@angular/forms';
 
-export function emailValid(): ValidatorFn {
-   const regExp = new RegExp(`^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$`);
-   return (control) => {
-     const isInvalid = control.value === '' || regExp.test(control.value);
-     return isInvalid ? null : { emailValid: true };
-   };
- }
+// export function emailValid(): ValidatorFn {
+//    const regExp = new RegExp(`^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`);
+//    return (control) => {
+//      const isInvalid = control.value === '' || regExp.test(control.value);
+//      return isInvalid ? null : { emailValid: true };
+//    };
+//  }
+
+import { ValidatorFn, AbstractControl } from '@angular/forms';
+
+export function emailValidator(): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    let email = control.value;
+    if (!email) {
+      // if control is empty return null
+      return null;
+    }
+
+    // Regular expression for email validation
+    const regex = new RegExp(`^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}$`);
+    const valid = regex.test(email);
+    // return null if email is valid, otherwise return an object with validation error
+    return valid ? null : { 'emailValid': true };
+  };
+}
