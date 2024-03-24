12cfaf9223646e2d0699e3f184e1acb209694e65
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2024-03-24 23:23:09 +0200

    Set cookie to maxAge: 3000 * 60 * 60,

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -5,7 +5,7 @@ const registerUser = async (req, res) => {
    try {
       const result = await authService.register(userData);
       const { token, username, id } = result;
-      res.cookie('auth', token, {httpOnly: true});
+      res.cookie('auth', token, {maxAge: 3000 * 60 * 60, httpOnly: true});
       res.status(200).json({ message: 'Registration successful', username, id });
    } catch (err) {
       const errMsg = err.message;
@@ -22,7 +22,7 @@ const loginUser = async (req, res) => {
    try {
       const result = await authService.login(loginData);
       const { token, username, id } = result;
-      res.cookie('auth', token, {httpOnly: true});
+      res.cookie('auth', token, {maxAge: 3000 * 60 * 60,httpOnly: true});
       res.status(200).json({ message: 'Login successful', username, id });
    } catch (err) {
       const errMsg = err.message;
