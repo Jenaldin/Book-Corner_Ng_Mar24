d41f61b1d09673b68e154892ff832990130acaf9
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2024-03-30 04:38:02 +0200

    Created look & feel for all-comments component

--- client/src/app/comments/all-comments/all-comments.component.ts
+++ client/src/app/comments/all-comments/all-comments.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AddCommentComponent } from '../add-comment/add-comment.component';
 
 @Component({
   selector: 'app-all-comments',
@@ -7,11 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllCommentsComponent implements OnInit{
   isLoading: boolean = true;
+  panelOpenState = false;
+
+  constructor(public dialogBox: MatDialog) {}
 
   ngOnInit(): void {
     setTimeout(() => {
       this.isLoading = false;
     }, 500);
+  }
+
+  newComment() {
+    const dialogRef = this.dialogBox.open(AddCommentComponent, {
+      disableClose: true 
+    });
+  
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
 
 }
