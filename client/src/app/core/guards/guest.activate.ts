664d141cae2570d27509f03461c5e50ee01cc356
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2024-03-31 16:15:10 +0300

    Cleaned up guest guard on FE

--- client/src/app/core/guards/guest.activate.ts
+++ client/src/app/core/guards/guest.activate.ts
@@ -1,13 +1,8 @@
-import { Injectable, inject } from '@angular/core';
+import { inject } from '@angular/core';
 import {
-  ActivatedRouteSnapshot,
-  CanActivate,
   CanActivateFn,
   Router,
-  RouterStateSnapshot,
-  UrlTree,
 } from '@angular/router';
-import { Observable } from 'rxjs';
 import { UserService } from 'src/app/core/services/user.service';
 
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -29,31 +24,3 @@ export function guestActive(): CanActivateFn{
           return true;
   }
 }
-
-// @Injectable({ providedIn: 'root' })
-// export class GuestActive implements CanActivate {
-//   constructor(
-//     private userApi: UserService,
-//     private snackBar: MatSnackBar,
-//     private router: Router
-//   ) {}
-
-//   canActivate(
-//     route: ActivatedRouteSnapshot,
-//     state: RouterStateSnapshot,
-//   ):
-//     | boolean
-//     | UrlTree
-//     | Observable<boolean | UrlTree>
-//     | Promise<boolean | UrlTree> {
-//     if (this.userApi.isLoggedIn) {
-//       this.snackBar.open(
-//         `You are already logged in, you can't use this path now. First logout.`,
-//         'Close',
-//         { duration: 10000 },
-//       );
-//       return this.router.createUrlTree(['/']);
-//     }
-//     return true;
-//   }
-// }
