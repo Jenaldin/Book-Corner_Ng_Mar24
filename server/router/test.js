6b2f344c6b11d7b4b41549d6f145966470a39fe6
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2024-04-02 03:34:47 +0300

    Corrected some messages on BE routes that are not used

--- server/router/test.js
+++ server/router/test.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 router.get('/', function (req, res) {
    res.send(`<div><h3>Speak Friend And Enter!</h3></div>
-   <div><h3>You have made it to the test location, the API routes work!</h3></div>`);
+   <div><h3><i>- Gandalf the Gray, Lord of the Rings, J.R.R. Tolkien</i></h3></div>
+   <br>
+   <div><h3>You have reached the test location, confirming that the API routes are functional.</h3></div>
+   <div><h3>Please use the front-end application at http://localhost:4200</h3></div>`);
 })
 
 module.exports = router
--- server/routes.js
+++ server/routes.js
@@ -1,13 +1,20 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-   res.send(`<h3>You Shall Not Pass!!!</h3>`);
+   res.send(`<h3>You Shall Not Pass!!!</h3>
+   <div><h3><i>- Gandalf the Gray, Lord of the Rings, J.R.R. Tolkien</i></h3></div>
+   <br>
+   <div><h3>Welcome to the API. This is the server.</h3></div>
+   <div><h3>Please use the front-end application at http://localhost:4200</h3></div>`
+   );
 });
 
 router.get('*', (req, res) => {
    res.status(404).send(`<div><h3>Not All Who Wonder Are Lost...</h3></div>
-   <div><h3>But in your case, you are lost, try another path.</h3></div>
-   <div><h3>Or better, use app FE at http://localhost:4200, thanks!</h3></div>`);
+   <div><h3><i>- Bilbo Baggins, Lord of the Rings, J.R.R. Tolkien</i></h3></div>
+   <br>
+   <div><h3>The requested resource could not be found on this server.</h3></div>
+   <div><h3>Please use the front-end application at http://localhost:4200</h3></div>`);
  });
 
 module.exports = router;
